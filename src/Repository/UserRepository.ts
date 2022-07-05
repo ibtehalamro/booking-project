f2bc78cfac69a830ef70c52029afd008efe08c6f
ibtehal <ibtehalamro11@gmail.com>
2022-07-05 10:50:08 +0300

    main

--- src/Repository/UserRepository.ts
+++ src/Repository/UserRepository.ts
@@ -1,31 +1,25 @@
 import { Connection } from "mysql";
 import User from "../Models/User.js";
- 
+
 export class UserRepository {
+  private users = new Map<number, User>([
+    [1, new User("ibtehal.email.com", "1234", "ibtehal")],
+    [1, new User("rahaf.email.com", "1234", "rahaf")],
+    [1, new User("tala.email.com", "1234", "tala")],
+  ]);
+
   private connection: Connection;
   constructor(connection: Connection) {
     this.connection = connection;
   }
 
 
-  getUser = (email: string):Promise<User> => {
-    return new Promise((resolve, reject)=>{
-        this.connection.query( `SELECT * FROM users WHERE email = '${email}'`,  (error, elements)=>{
-            if(error){
-                return reject(error);
-            }
-            return resolve(elements );
-        });
-    });
-};
 
-async   getUserByEmail(email: string):Promise<User | null> {
-    let user=null;
+  async getUserByEmail(email: string): Promise<User | null> {
+    let user = null;
     console.log("before query");
-  user = await this.getUser(email)
-  
+    user = await this.getUser(email);
     console.log("after query");
-
     return user;
   }
 }
